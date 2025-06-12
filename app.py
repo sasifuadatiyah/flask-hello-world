17ca6dcf29e8e35049f4bd9300fe91fa0799f238
Sasi Atiyah <sasi.fouad@gmail.com>
2025-06-12 07:52:20 +0400

    changes done

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1><p>Welcome to your Flask application!</p>'
+    return '<h1>Hello, World!</h1><p>Welcome to your Flask application! Sasi </p>'
 
 @app.route('/hello/<name>')
 def hello_name(name):
-    return f'<h1>Hello, {name}!</h1><p>Nice to meet you!</p>'
+    return f'<h1>Hello, {name}!</h1><p>Nice to meet you Sasi!</p>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5555) 
